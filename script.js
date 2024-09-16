e1e1b6ff8531d9b20fb1553f84872d9713f7d9f2
mohamed hossam <101831478+mohamed1092001@users.noreply.github.com>
2024-09-16 23:13:12 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+$(".ar-btn").click(function () {
+    $("section").slideUp(800);
+    $(".page-2").slideDown(800);
+})
+$(".back-f-p2").click(function () {
+    $("section").slideUp(800);
+    $(".page-1").slideDown(800);
+})
+// #######
+$(".motargem-btn").click(function () {
+    $("section").slideUp(800);
+    $(".motargem").slideDown(800);
+})
+
+$(".moltaka-btn").click(function () {
+    $("section").slideUp(800);
+    $(".moltaka").slideDown(800);
+})
+
+$(".mobadra-btn").click(function () {
+    $("section").slideUp(800);
+    $(".mobadra").slideDown(800);
+})
+$(".dalil-btn").click(function () {
+    $("section").slideUp(800);
+    $(".dalil").slideDown(800);
+})
+$(".korsy-btn").click(function () {
+    $("section").slideUp(800);
+    $(".korsy").slideDown(800);
+})
+$(".marsad-btn").click(function () {
+    $("section").slideUp(800);
+    $(".marsad").slideDown(800);
+})
+$(".trainning1-btn").click(function () {
+    $("section").slideUp(800);
+    $(".trainning1").slideDown(800);
+})
+$(".quality-btn").click(function () {
+    $("section").slideUp(800);
+    $(".quality").slideDown(800);
+})
+$(".ershaad-btn").click(function () {
+    $("section").slideUp(800);
+    $(".ershaad").slideDown(800);
+})
+$(".trainning2-btn").click(function () {
+    $("section").slideUp(800);
+    $(".trainning2").slideDown(800);
+})
+$(".quality2-btn").click(function () {
+    $("section").slideUp(800);
+    $(".quality2").slideDown(800);
+})
+
+// ##########
+
+$(".back-f-details").click(function () {
+    $("section").slideUp(800);
+    $(".page-2").slideDown(800);
+})
+
+$(".back-btn").click(function () {
+    $(".details-page video").trigger("pause")
+    $(".details-page video").prop('currentTime', 0);
+})
